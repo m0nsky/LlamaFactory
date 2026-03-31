4e5bf79142cd705eca591a0800461f3c893de141
m0nsky <ramonkroes@live.co.uk>
2026-03-31 13:14:33 +0200

    Guard processor access in Qwen2VL, Qwen3VL, and GLM4V plugins for text-only models

--- src/llamafactory/data/mm_plugin.py
+++ src/llamafactory/data/mm_plugin.py
@@ -1571,9 +1571,12 @@ class Qwen2VLPlugin(BasePlugin):
         self._validate_messages(messages, images, videos, audios)
         num_image_tokens, num_video_tokens = 0, 0
         messages = deepcopy(messages)
-        image_processor: BaseImageProcessor = getattr(processor, "image_processor")
+        if processor is not None:
+            image_processor: BaseImageProcessor = getattr(processor, "image_processor")
+            merge_length: int = getattr(image_processor, "merge_size") ** 2
+        else:
+            merge_length = 1
 
-        merge_length: int = getattr(image_processor, "merge_size") ** 2
         if self.expand_mm_tokens:
             mm_inputs = self._get_mm_inputs(images, videos, audios, processor)
             image_grid_thw = mm_inputs.get("image_grid_thw", [])
@@ -1667,11 +1670,14 @@ class Qwen3VLPlugin(Qwen2VLPlugin):
         self._validate_messages(messages, images, videos, audios)
         num_image_tokens, num_video_tokens = 0, 0
         messages = deepcopy(messages)
-        image_processor: BaseImageProcessor = getattr(processor, "image_processor")
-        video_processor: BaseImageProcessor = getattr(processor, "video_processor")
-
-        image_merge_length: int = getattr(image_processor, "merge_size") ** 2
-        video_merge_length: int = getattr(video_processor, "merge_size") ** 2
+        if processor is not None:
+            image_processor: BaseImageProcessor = getattr(processor, "image_processor")
+            video_processor: BaseImageProcessor = getattr(processor, "video_processor")
+            image_merge_length: int = getattr(image_processor, "merge_size") ** 2
+            video_merge_length: int = getattr(video_processor, "merge_size") ** 2
+        else:
+            image_merge_length = 1
+            video_merge_length = 1
         if self.expand_mm_tokens:
             mm_inputs = self._get_mm_inputs(images, videos, audios, processor)
             image_grid_thw = mm_inputs.get("image_grid_thw", [])
@@ -1781,9 +1787,12 @@ class GLM4VPlugin(Qwen2VLPlugin):
         self._validate_messages(messages, images, videos, audios)
         num_image_tokens, num_video_tokens = 0, 0
         messages = deepcopy(messages)
-        image_processor: BaseImageProcessor = getattr(processor, "image_processor")
+        if processor is not None:
+            image_processor: BaseImageProcessor = getattr(processor, "image_processor")
+            merge_length: int = getattr(image_processor, "merge_size") ** 2
+        else:
+            merge_length = 1
 
-        merge_length: int = getattr(image_processor, "merge_size") ** 2
         if self.expand_mm_tokens:
             mm_inputs = self._get_mm_inputs(images, videos, audios, processor)
             image_grid_thw = mm_inputs.get("image_grid_thw", [])
